56f819a63734271f7e3363951eae70f960b17bce
Abhik Ranjan Khanra and Kiran Gangadharan and Udit Kumar <abhik.rk+me+yudistrange@gmail.com>
2017-09-01 15:56:56 +0530

    Added HH:MM:SS display for timers

--- src/cljs/time_tracker_web_nxt/views.cljs
+++ src/cljs/time_tracker_web_nxt/views.cljs
@@ -1,14 +1,23 @@
 (ns time-tracker-web-nxt.views
-  (:require [re-frame.core :as re-frame]))
+  (:require [re-frame.core :as re-frame]
+            [goog.string :as gs]
+            [goog.string.format]))
 
 (defn add-timer []
   [:button
    {:type "input" :on-click #(re-frame/dispatch [:add-timer (rand-int 30)])}
    "Add a Timer"])
 
+(defn time-display [elapsed-seconds]
+  (let [minutes (-> (quot elapsed-seconds 60)
+                    (mod 60))
+        hours (quot minutes 60)
+        seconds (mod elapsed-seconds 60)]
+    (gs/format "%02d:%02d:%02d" hours minutes seconds)))
+
 (defn timer [[k {:keys [id elapsed state]}] ]
   [:div 
-   [:p "Timer " id " has been running for " elapsed " seconds as " state]
+   [:p "Timer " id " has been running for " (time-display elapsed) " seconds as " state]
    (condp = state
      :paused [:button {:on-click #(re-frame/dispatch [:start-timer id])} "Start Timer"]
      :running [:button {:on-click #(re-frame/dispatch [:stop-timer id])} "Stop Timer"]
