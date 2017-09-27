da3d14d27350c670afcd10c259b2f4066f4b1b18
trycatcher <abhik.rk@gmail.com>
2017-09-27 16:19:27 +0530

    Add logging using Timbre

--- src/cljs/time_tracker_web_nxt/config.cljs
+++ src/cljs/time_tracker_web_nxt/config.cljs
@@ -1,4 +1,11 @@
-(ns time-tracker-web-nxt.config)
+(ns time-tracker-web-nxt.config
+  (:require
+   [taoensso.timbre :as timbre]
+   [time-tracker-web-nxt.env-vars :as env]))
 
 (def debug?
   ^boolean goog.DEBUG)
+
+;; Set the logging level
+;; Possible values are #{:trace :debug :info :warn :error :fatal :report}
+(timbre/set-level! (keyword (:log-level env/env)))
