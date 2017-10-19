49f0cfcab61cc3d50d72e5971290f9af7a85f25a
Kiran Gangadharan <me@kirang.in>
2017-10-19 16:29:21 +0530

    Add/Improve tests

--- test/cljs/time_tracker_web_nxt/core_test.cljs
+++ test/cljs/time_tracker_web_nxt/core_test.cljs
@@ -9,7 +9,7 @@
             [cljs-time.core :as t-core]
             [time-tracker-web-nxt.utils :as utils]))
 
-(def test-user
+(def user
   {:name "Kiran"
    :image-url ""
    :token "eyJhbGciOiJSUzI1NiIsImtpZCI6IjBlNzhlN2FlZj"
@@ -20,6 +20,21 @@
    :add-user
    (fn [db [_ user]]
      (assoc db :user user)))
+
+  (rf/reg-event-fx
+   :set-clock
+   (fn [timer-id]
+     {:called-with timer-id}))
+
+  (rf/reg-event-fx
+   :clear-clock
+   (fn [interval-id]
+     {:called-with interval-id}))
+
+  (rf/reg-fx
+   :ws/send
+   (fn [_]
+     "Stub out websocket send"))
 
   (rf/dispatch [:initialize-db])
   (rf/dispatch [:add-user test-user]))
@@ -40,12 +55,61 @@
 
      (rf/dispatch [:add-timer-to-db new-timer])
 
-     (is (= 1 (count @timers)))
-     (is (some #(= (:id new-timer) %) (keys @timers)))
-     (is (= new-timer (get @timers (:id new-timer)))))))
+     (testing "Adding new timer should add it to re-frame db"
+       (is (= 1 (count @timers))))
+
+     (testing "Timer added to db should be same as the one created"
+       (is (= new-timer (get @timers (:id new-timer))))))))
 
 (defn check-contains [a-map key]
   (some #(= key %) (keys a-map)))
+
+(defn run-flow [events param]
+  (doall
+   (map #(rf/dispatch [% param]) events)))
+
+(defn make-timer [opts]
+  (let [now (t-core/now)
+        created-at (utils/timer-created-time (str (js/Date.)) now)
+        default {:id 1
+                 :project-id 1
+                 :started-time nil
+                 :duration 0
+                 :time-created created-at
+                 :notes "Foo"
+                 :type "create"}]
+    (merge default opts)))
+
+(defn make-n-timers [n]
+  (reduce #(assoc %1 %2 (make-timer {:id %2}))
+          {}
+          (range 1 (inc n))))
+
+(deftest start-timer-test
+  (rf-test/run-test-sync
+   (testing "user can start a timer"
+     (let [timer    (make-timer {:id 1})
+           timer-id (:id timer)]
+       (test-fixtures)
+
+       (run-flow [:add-timer-to-db :start-timer] timer)
+
+       (let [timers (rf/subscribe [:timers])]
+         (testing "starting timer changes it's state from paused to running"
+           (is (= :running (get-in @timers [timer-id :state])))))))))
+
+(deftest stop-timer-test
+  (rf-test/run-test-sync
+   (testing "user can stop a timer"
+     (let [timer    (make-timer {:id 2})
+           timer-id (:id timer)
+           timers (rf/subscribe [:timers])]
+       (test-fixtures)
+
+       (run-flow [:add-timer-to-db :start-timer :stop-timer] timer)
+
+       (testing "stopping timer changes it's state from running to paused"
+         (is (= :paused (get-in @timers [2 :state]))))))))
 
 (deftest create-and-start-timer-test
   (testing "user can create a new timer"
