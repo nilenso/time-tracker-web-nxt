34983321b0538fd295f21fcd3380fe9a922ef152
Harsh Gupta <harsh@nilenso.com>
2018-04-25 11:09:28 +0530

    reset config.cljs

--- src/cljs/time_tracker_web_nxt/config.cljs
+++ src/cljs/time_tracker_web_nxt/config.cljs
@@ -8,9 +8,9 @@
 (goog-define ^boolean debug? false)
 
 (def dev
-  {:client-id "673127655477-iidn2bttouhpvh2a4l6l8e7cp1pr42uo.apps.googleusercontent.com" 
+  {:client-id "163789226845-gfp9na27iac63lnrb81hsdkvf5log85i.apps.googleusercontent.com"
    :scope "profile email"
-   :conn-url "ws://localhost:8000/api/timers/ws-connect/"
+   :conn-url "ws://localhost/api/timers/ws-connect/"
    :log-level "debug"})
 
 (def prod
