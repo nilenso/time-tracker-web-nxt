c5105270eec09dc67972fd248682e7abf060def8
Samrat Man Singh <samrat@nilenso.com>
2018-05-07 14:32:50 +0530

    Add clients, tasks and selected-* keys to app-db spec
    Co-authored-by: Harsh Gupta <harsh@nilenso.com>

--- src/cljs/time_tracker_web_nxt/db.cljs
+++ src/cljs/time_tracker_web_nxt/db.cljs
@@ -7,20 +7,39 @@
 (s/def ::show-user-menu? boolean?)
 (s/def ::timer-date inst?)
 
-;; User
+;; Common
+(s/def ::id int?)
 (s/def ::name string?)
+
+;; User
 (s/def ::image-url string?)
 (s/def ::token string?)
 (s/def ::signed-in? boolean?)
 (s/def ::user (s/nilable (s/keys :req-un [::name ::image-url ::token ::signed-in?])))
 
+;; Client
+(s/def ::gstin (s/or ::gstin-str (s/and string? #(= 15 (count %)))
+                     ::empty empty?))
+(s/def ::pan (s/or ::pan-str (s/and string? #(= 10 (count %)))
+                   ::empty empty?))
+(s/def ::client (s/keys :req-un [::id ::name]
+                        :opt-un [::gstin ::pan]))
+(s/def ::clients (s/coll-of ::client))
+(s/def ::selected-client-id int?)
+
 ;; Project
-(s/def ::id int?)
-(s/def ::project (s/keys :req-un [::id ::name]))
+(s/def ::client_id int?)
+(s/def ::project (s/keys :req-un [::id ::name ::client_id]))
 (s/def ::projects (s/coll-of ::project))
+(s/def ::selected-project-id int?)
+
+;; Task
+(s/def ::project_id int?)
+(s/def ::task (s/keys :req-un [::id ::name ::project_id]))
+(s/def ::tasks (s/coll-of ::task))
+(s/def ::selected-task-id int?)
 
 ;; Timer
-(s/def ::id int?)
 (s/def ::task-id int?)
 (s/def ::duration (s/nilable int?))
 (s/def ::state keyword?)
@@ -35,15 +54,19 @@
 (s/def ::timers (s/every-kv int? ::timer))
 
 ;; App DB
-(s/def ::db (s/keys :req-un [::app-name ::projects ::show-create-timer-widget?
+(s/def ::db (s/keys :req-un [::app-name ::clients ::projects ::tasks
+                             ::show-create-timer-widget?
                              ::timers ::user ::conn ::show-user-menu?]
-                    :opt-un [::intervals ::timer-date]))
+                    :opt-un [::intervals ::timer-date ::selected-client-id
+                             ::selected-project-id ::selected-task-id]))
 
 (def default-db
   {:app-name                  "the future Time Tracker"
    :timers                    {}
    :timer-date                (js/Date. (.setHours (js/Date.) 0 0 0 0))
+   :clients                   []
    :projects                  []
+   :tasks                     []
    :conn                      []
    :app-user-id               nil
    :show-create-timer-widget? false
